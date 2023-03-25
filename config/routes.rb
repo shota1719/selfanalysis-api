ef91025fa0de154bee62b064f99071d4eedb7159
Shirai <shotashirai1719@gmail.com>
2023-03-25 16:35:45 +0900

    fix/20230325/registration_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'Patients', at: 'auth'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1, defaults: {format: :json} do
+      mount_devise_token_auth_for 'Patients', at: 'auth', controllers: {
+        registrations: "api/vi/auth/registrations"
+      }
+    end
+  end
 end
---
+++ test/controllers/api/vi/auth/registrations_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::Vi::Auth::RegistrationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
